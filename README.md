d4e84158c131df2a5f3b1e64db2f796903abc8f5
Moacyr Pereira <moacyr.pereira@allin.com.br>
2022-08-04 17:31:14 -0300

    Projeto concluído.

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center">Gerador de QR Code</h1>
+
+<img align="center" style="width: 400px" src="./img/2.png"/>
+
+
+<h2 id="tecnologias" style="margin-top: 25px;">Tecnologias<h2>
+<p style="font-size: 13px;">Este projeto foi desenvolvido ultilizando: HTML, CSS, JavaScript consumido uma API da <a href="https://goqr.me/api/">QR Code Generator.</a></p>
+
+<h4>Veja como ficou. <a href="https://qrcode.iron-code.com/">Clique aqui</a></h4>
--- js/config.js
+++ js/config.js
@@ -1,6 +1,23 @@
 const container = document.querySelector(".container"),
-geradorBtn = container.querySelector(".form button");
+qrInput = container.querySelector(".form input"),
+geradorBtn = container.querySelector(".form button"),
+qrImg = container.querySelector(".qr-code img");
 
 geradorBtn.addEventListener("click", () => {
-    container.classList.add("active");
-})
+
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    geradorBtn.innerText = "Gerando QR code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+
+    qrImg.addEventListener("load", () => {
+        container.classList.add("active");
+        geradorBtn.innerText = "Gerar QR Code.";
+    });  
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value) {
+        container.classList.remove("active");
+    }
+});
